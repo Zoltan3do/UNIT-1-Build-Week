6a769884237b4c4254b02bf182b41dbad2939e41
nome-git <168424276+AntonioNorfo@users.noreply.github.com>
2024-07-03 13:16:43 +0200

    Sistemata quest/risposte

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -4,36 +4,47 @@ const rudeCheckbox = document.querySelector("#rude-checkbox");
 
 window.addEventListener("load", init());
 function init() {
-    initStyles();
+  initStyles();
 }
 
 function initStyles() {
-    checkmark.style.display = "none";
-    bottone.setAttribute("disabled", "true");
-    bottone.style.cursor = "auto";
-    bottone.style.opacity = 0.6
+  checkmark.style.display = "none";
+  bottone.setAttribute("disabled", "true");
+  bottone.style.cursor = "auto";
+  bottone.style.opacity = 0.6;
+  bottone.style.backgroundColor = "gray";
+  bottone.addEventListener("mouseover", () => {
+    bottone.style.boxShadow = "none";
+  });
 }
 
 function checkbox() {
-    checkmark.style.display = "inline-block";
-    bottone.setAttribute("disabled", "false");
-    bottone.style.cursor = "pointer";
-    bottone.style.opacity = 1
-    rudeCheckbox.style.color = "#c2138e";
-
+  checkmark.style.display = "inline-block";
+  bottone.setAttribute("disabled", "false");
+  bottone.style.cursor = "pointer";
+  bottone.style.opacity = 1;
+  rudeCheckbox.style.color = "#c2138e";
+  bottone.style.backgroundColor = "#02ffff";
+  bottone.addEventListener("mouseover", () => {
+    bottone.style.boxShadow =
+      "0 0 20px rgba(2, 255, 255, 0.5), 0 0 20px rgba(2, 255, 255, 0.5), 0 0 20px rgba(2, 255, 255, 0.5)";
+  });
+  bottone.addEventListener("mouseout", () => {
+    bottone.style.boxShadow = "none";
+  });
 }
 
 // variabile di stato
 let togglestate = true;
 rudeCheckbox.addEventListener("click", function () {
-    if (togglestate) {
-        checkbox();
-    } else {
-        initStyles()
-    }
-    togglestate = !togglestate;
+  if (togglestate) {
+    checkbox();
+  } else {
+    initStyles();
+  }
+  togglestate = !togglestate;
 });
 
 function cambiaPagina() {
-    window.location.href = "test.html";
+  window.location.href = "test.html";
 }
